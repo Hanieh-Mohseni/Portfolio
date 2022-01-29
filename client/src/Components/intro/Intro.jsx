f1eb3633f0358f791931df2bccf06e933e4b995e
Hanieh-Mohseni <mohseni.hanieh@gmail.com>
2022-01-28 23:54:14 -0500

    Intro section

--- client/src/Components/intro/Intro.jsx
+++ client/src/Components/intro/Intro.jsx
@@ -4,7 +4,20 @@ import './intro.scss';
 const Intro=()=> {
   return(
        <div className='intro' id="intro">
+         <div className='left'>
+           <div className='imgContainer'>
+             <img src='assets/Myphoto.png' alt=''/>
+           </div>
 
+         </div>
+         <div className='right'>
+           <div className='wrapper'>
+             <h2>Hi There, I'm</h2>
+             <h1>Hanih Mohseni</h1>
+             <h3>Full-Stack Web Developer</h3>
+           </div>
+
+         </div>
        </div>
   )
 }
