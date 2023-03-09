4b49cde5585d5468fe63b231cb9a490e48af00a6
Hanieh <mohseni.hanieh@gmail.com>
2023-03-08 23:56:23 -0500

    Add small changes in CSS

--- client/src/Components/portfolio/resume/resumeSections/resumeParts/resume-experiance/ResumeExperience.jsx
+++ client/src/Components/portfolio/resume/resumeSections/resumeParts/resume-experiance/ResumeExperience.jsx
@@ -13,12 +13,11 @@ const ResumeExperience = () => {
 
         <h2>Java Software Developer</h2>
         <h3>Wiley Edge | Oct 2022 - Present  </h3>
-        <Myp> Developed a broad set of technical skills including:
-            ❏ Design, develop, and test of Java-based full-stack applications
-            ❏ CRUD full-stack application with an agile approach
-            ❏ Create a full test unit for the service layer and DAOs in applications
-            ❏ Object-Oriented Programming (OOP), MVC design pattern, RESTful services
-            ❏ Java, SQL, Spring Boot, Maven, JDBC, Junit5, Spring MVC, MySQL, Git/GitHub, AWS, familiarity with high-level financial concepts</Myp>
+        <h3> Developed a broad set of technical skills including:
+            | Design, develop, and test of Java-based full-stack applications
+            | CRUD full-stack application with an agile approach
+            | MVC design pattern, RESTful services
+            | Java, SQL, Spring Boot, Maven, JDBC, Junit5, Spring MVC, MySQL, AWS, high-level financial concepts</h3>
 
 
         <h2>E-learning Developer </h2>
@@ -29,6 +28,7 @@ const ResumeExperience = () => {
 
         <h2>E-Content Developer</h2>
         <h3>E-Learning Center, Iran University of Science and Technology, Tehran, Iran | 2007 - 2012 </h3>
+        <h3>Produced more than ten Courseware projects</h3>
 
         <h2>Instructor </h2>
         <h3>University of Applied Science and Technology-Dehkhoda | 2007 - 2008 </h3>
